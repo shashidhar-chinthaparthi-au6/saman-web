cd661e35aab26bd76a59ee1bd2c1516c2de978a8
Shashi <shashidhar.rao@newstreettech.com>
2024-09-04 08:46:49 +0530

    changed packkages

--- src/store.js
+++ src/store.js
@@ -1,28 +1,33 @@
 // src/store.js
 
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk'; // Import thunk correctly
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import { categoryReducer, productReducer } from './reducers/adminReducers';
 import { subcategoryAddReducer, subcategoryListReducer } from './reducers/subcategoryReducers';
 import userReducer from './reducers/userReducer';
 
 // Combine your reducers into a root reducer
 const rootReducer = combineReducers({
-    user: userReducer,
+  user: userReducer,
   categoryList: categoryReducer,
   productList: productReducer,
   subcategoryList: subcategoryListReducer,
   subcategoryAdd: subcategoryAddReducer,
 });
 
-// Create an array of middlewares
+// Create an array of middleware
 const middleware = [thunk];
+
+// Setup Redux DevTools manually with a fallback for non-development environments
+const composeEnhancers =
+  (typeof window !== 'undefined' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
 // Create the Redux store with the root reducer, middleware, and DevTools integration
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(...middleware))
+  composeEnhancers(applyMiddleware(...middleware))
 );
 
 // Export the store
